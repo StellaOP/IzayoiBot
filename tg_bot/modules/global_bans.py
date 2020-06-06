0cb9b7d8e7f6cb5d1d547a54287588aff36a3e8b
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-06-06 13:56:59 +0530

    Update global_bans.py
    KEK imported wrong sql file

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -19,18 +19,20 @@ import html
 import time
 import spamwatch
 from io import BytesIO
-from typing import List
+from typing import Optional, List
 
-from telegram import Update, Bot, ParseMode
-from telegram.error import BadRequest  #,  TelegramError
+from telegram import Message, Update, Bot, User, Chat, ParseMode
+from telegram.error import BadRequest, TelegramError
 from telegram.ext import run_async, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
-import tg_bot.modules.sql.antispam_sql as sql
-from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, MESSAGE_DUMP, STRICT_ANTISPAM, spamwtc
+import tg_bot.modules.sql.global_bans_sql as sql
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, MESSAGE_DUMP, STRICT_GBAN, spamwtc
 from tg_bot.modules.helper_funcs.chat_status import user_admin, is_user_admin
-from tg_bot.modules.helper_funcs.extraction import extract_user_and_text
+from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
 from tg_bot.modules.helper_funcs.filters import CustomFilters
+from tg_bot.modules.helper_funcs.misc import send_to_list
+from tg_bot.modules.sql.users_sql import get_all_chats
 #from tg_bot.modules.helper_funcs.misc import send_to_list
 # from tg_bot.modules.sql.users_sql import get_all_chats
 
